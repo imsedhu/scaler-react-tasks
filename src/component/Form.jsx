b61c82ae8182d37f03b8042e8788c0c06034b423
imsedhu <sedhuramansedhu17@gmail.com>
2024-07-24 21:25:07 +0530

    second commit

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,18 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState({ firstname: "", lastname: "" });
 
-  const [name, setName] = useState({firstname: '', lastname: ''})
+  function handleFirstName(e) {
+    setName(e.target.value);
+    console.log(e.target.value);
+  }
+
+  function handlelastName(e) {
+    setName(e.target.value);
+    console.log(e.target.value);
+  }
   return (
     <div>
-    <h1>Form pratice</h1>
-    <h2>{name}</h2>
-    <label>firstname</label>
-    <input type="text"  id="firstname" onChange={(e)=>{e.target.value}} />
-    <label>lastname</label>
-    <input type="text"  id="lastname" />
-    </div>
-  )
-}
+      <h1>Form pratice</h1>
+      <h2>
+        {name.firstname} -
+        {name.lastname}
+      </h2>
+      <label>firstname</label>
+      <input
+        type="text"
+        id="firstname"
+        onChange={(e) => {
+          handleFirstName(e);
+        }}
+      />
+      <label>lastname</label>
+      <input
+        type="text"
+        id="lastname"
 
-export default Form
+        onChange={(e) => {
+          handlelastName(e);
+        }}
+      />
+    </div>
+  );
+};
+
+export default Form;
